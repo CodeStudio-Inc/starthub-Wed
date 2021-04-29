36b5e36db899401210f5330fc26cf50e6484b809
Stuartkal <Stuartkal@gmail.com>
2021-04-29 17:28:46 +0300

    button-resize

--- src/components/Layout/Home/Card.js
+++ src/components/Layout/Home/Card.js
@@ -69,64 +69,62 @@ const Card = (props) => {
                 <Sidebar />
             </div>
             <div className="right-column-content">
-                <div className="cards-right-column-content">
-                    <div className="boards-header">
-                        <h2>{boardName}</h2>
-                        <div className="edit-row">
-                            <div className="search">
-                                <input
-                                    type="text"
-                                    placeholder="Search"
-                                />
-                                <Search style={{ fontSize: '20px', color: 'rgba(0, 0, 0, 0.1)' }} />
-                            </div>
-                            <div className="separator" />
-                            <AddBoxIcon onClick={() => setOpen(true)} className="add-icon" style={{ fontSize: '40px', color: 'rgba(0, 0, 0, 0.1)' }} />
+                <div className="boards-header">
+                    <h2>{boardName}</h2>
+                    <div className="edit-row">
+                        <div className="search">
+                            <input
+                                type="text"
+                                placeholder="Search"
+                            />
+                            <Search style={{ fontSize: '20px', color: 'rgba(0, 0, 0, 0.1)' }} />
                         </div>
+                        <div className="separator" />
+                        <AddBoxIcon onClick={() => setOpen(true)} className="add-icon" style={{ fontSize: '40px', color: 'rgba(0, 0, 0, 0.1)' }} />
                     </div>
-                    <div className="milestone-row">
-                        {lists.map((list, index) => (
-                            <div className="list-card" key={list._id}>
-                                <div className="list-header">
-                                    <h3>{list.name}</h3>
-                                    <DeleteIcon onClick={() => dispatch(actionCreators.deleteList(list._id))} className="delete-icon" />
-                                </div>
-                                {cards.map(card => {
-                                    if (list._id === card.listId)
-                                        return (
-                                            <div className="card-column" key={card._id}>
-                                                <div className="card-text">
-                                                    <h5>{card.name}</h5>
-                                                </div>
-                                                <button onClick={() => dispatch(actionCreators.deleteCard(card._id))}>remove</button>
-                                            </div>
-                                        )
-                                })}
-                                {show && activeListId === list._id ? <div className="add-card">
-                                    <input
-                                        placeholder="Enter Card Title"
-                                        type="text"
-                                        value={cardName}
-                                        onChange={(e) => setCardName(e.target.value)}
-                                    />
-                                    <button onClick={() => {
-                                        dispatch(actionCreators.createCard(boardId, list._id, cardName, (res) => {
-                                            if (res.success === true) {
-                                                setTimeout(() => {
-                                                    dispatch(actionCreators.getCardsOnBoard(boardId))
-                                                }, 2000)
-                                            }
-                                        }))
-                                        setCardName('')
-                                    }}>Add</button>
-                                </div> : null}
-                                { activeListId !== list._id ? <button onClick={() => {
-                                    setShow(true)
-                                    setActiveListId(list._id)
-                                }}>+ Add Card</button> : <button onClick={() => setShow(false)}>Close</button>}
+                </div>
+                <div className="milestone-row">
+                    {lists.map((list, index) => (
+                        <div className="list-card" key={list._id}>
+                            <div className="list-header">
+                                <h3>{list.name}</h3>
+                                <DeleteIcon onClick={() => dispatch(actionCreators.deleteList(list._id))} className="delete-icon" />
                             </div>
-                        ))}
-                    </div>
+                            {cards.map(card => {
+                                if (list._id === card.listId)
+                                    return (
+                                        <div className="card-column" key={card._id}>
+                                            <div className="card-text">
+                                                <h5>{card.name}</h5>
+                                            </div>
+                                            <button onClick={() => dispatch(actionCreators.deleteCard(card._id))}>remove</button>
+                                        </div>
+                                    )
+                            })}
+                            {show && activeListId === list._id ? <div className="add-card">
+                                <input
+                                    placeholder="Enter Card Title"
+                                    type="text"
+                                    value={cardName}
+                                    onChange={(e) => setCardName(e.target.value)}
+                                />
+                                <button onClick={() => {
+                                    dispatch(actionCreators.createCard(boardId, list._id, cardName, (res) => {
+                                        if (res.success === true) {
+                                            setTimeout(() => {
+                                                dispatch(actionCreators.getCardsOnBoard(boardId))
+                                            }, 2000)
+                                        }
+                                    }))
+                                    setCardName('')
+                                }}>Add</button>
+                            </div> : null}
+                            { activeListId !== list._id ? <button onClick={() => {
+                                setShow(true)
+                                setActiveListId(list._id)
+                            }}>+ Add Card</button> : <button onClick={() => setShow(false)}>Close</button>}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
