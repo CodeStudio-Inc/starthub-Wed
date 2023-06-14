59a388329206ad8224f570c470b883ff51b9d8f6
Stuartkal <Stuartkal@gmail.com>
2023-06-14 03:18:49 +0300

    okr ui redesign

--- src/components/Pages/LandingPages/TeamMember.js
+++ src/components/Pages/LandingPages/TeamMember.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Startups, ResourceFiles, Loans } from "../../Paths";
+import { Startups, ResourceFiles, OkrOverview, Loans } from "../../Paths";
 
 const TeamMember = ({ index }) => {
   return (
     <div style={{ width: "100%" }}>
       {index === 0 ? <Startups /> : null}
-      {index === 1 ? <ResourceFiles /> : null}
+      {index === 1 ? <OkrOverview /> : null}
       {/* {index === 2 ? <Loans /> : null} */}
     </div>
   );
